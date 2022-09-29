3cd7e10b3747d628f7427889a58b3a65178621bc
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-09-29 10:35:42 -0700

    2022 updates

--- test/integration/compiles.test.ts
+++ test/integration/compiles.test.ts
@@ -2,10 +2,8 @@ import { elementInnerText } from '@musical-patterns/utilities'
 import { page } from '../setup'
 
 describe('it compiles', (): void => {
-    it('since i have more than once deployed a blank screen of death due to the many @musical-patterns deps not matching / being up-to-date, confirm that the code compiles', async (done: DoneFn): Promise<void> => {
+    it('since i have more than once deployed a blank screen of death due to the many @musical-patterns deps not matching / being up-to-date, confirm that the code compiles', async (): Promise<void> => {
         expect(await elementInnerText(page, '#logo h1'))
             .toBe('Musical Patterns')
-
-        done()
     })
 })
